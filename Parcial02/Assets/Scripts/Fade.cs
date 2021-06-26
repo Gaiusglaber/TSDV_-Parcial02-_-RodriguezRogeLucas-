dddbbcad79d3824e59ce3971104c6d9fcaf8316b
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-06-26 13:02:07 -0300

    Discrimination of "SceneFade" and the SceneScript

---
+++ Parcial02/Assets/Scripts/Fade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    public static bool faded = false;
+    public void FadeComplete()
+    {
+        faded = true;
+    }
+}
--- Parcial02/Assets/Scripts/Intro.cs
+++ Parcial02/Assets/Scripts/Intro.cs
@@ -3,52 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
-public class MainMenu : MonoBehaviour
+public class Intro : MonoBehaviour
 {
-    public Animator animatorPlayButton;
     public Animator animatorScene;
     private int SceneIndex;
-    private bool pressed;
-    private bool videoEnded=false;
+    private bool videoEnded = false;
     private void Start()
     {
         GameObject video = GameObject.FindGameObjectWithTag("VideoObject");
-        if (video!=null)
-        video.GetComponent<VideoPlayer>().loopPointReached += CheckOver;
+        if (video != null)
+            video.GetComponent<VideoPlayer>().loopPointReached += CheckOver;
     }
     void Update()
     {
         if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey("enter") || Input.GetKey(KeyCode.Space) || videoEnded)
         {
-            StartCoroutine("PlayPressed");
+            FadeLevel(SceneManager.GetActiveScene().buildIndex + 1);
         }
-    }
-    public void Play()
-    {
-        pressed = true;
-    }
-    public void Exit()
-    {
-        Application.Quit();
+        if (Fade.faded)
+        {
+            Fade.faded = false;
+            SceneManager.LoadScene(SceneIndex);
+        }
     }
     public void FadeLevel(int SceneToTransition)
     {
         SceneIndex = SceneToTransition;
         animatorScene.SetTrigger("FadeOut");
     }
-    public void FadeComplete()
-    {
-        SceneManager.LoadScene(SceneIndex);
-    }
     void CheckOver(VideoPlayer vp)
     {
         videoEnded = true;
-    }
-    IEnumerator PlayPressed()
-    {
-        pressed = false;
-        yield return new WaitForSeconds(1);
-        FadeLevel(SceneManager.GetActiveScene().buildIndex + 1);
-        yield return null;
     }
 }
