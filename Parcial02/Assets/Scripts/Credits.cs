94275c09edbab45311d43de36b465be44d4d7128
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-06-26 14:43:56 -0300

    CreditsScene added

---
+++ Parcial02/Assets/Scripts/Credits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Credits : MonoBehaviour
+{
+    public Animator animatorScene;
+    private int SceneIndex;
+    void Update()
+    {
+        SceneFade();
+    }
+    public void SceneFade()
+    {
+        if (Fade.faded)
+        {
+            Fade.faded = false;
+            SceneManager.LoadScene(SceneIndex);
+        }
+    }
+    public void BackButton()
+    {
+        StartCoroutine("GoBackPressed");
+    }
+    public void FadeLevel(int SceneToTransition)
+    {
+        SceneIndex = SceneToTransition;
+        animatorScene.SetTrigger("FadeOut");
+    }
+    IEnumerator GoBackPressed()
+    {
+        yield return new WaitForSeconds(1);
+        FadeLevel(SceneManager.GetActiveScene().buildIndex - 1);
+        yield return null;
+    }
+}
--- Parcial02/Assets/Scripts/MainMenu.cs
+++ Parcial02/Assets/Scripts/MainMenu.cs
@@ -9,29 +9,34 @@ public class MainMenu : MonoBehaviour
     public Animator animatorScene;
     private int SceneIndex;
     private bool pressed;
-    private bool videoEnded=false;
-    private void Start()
-    {
-        GameObject video = GameObject.FindGameObjectWithTag("VideoObject");
-        if (video!=null)
-        video.GetComponent<VideoPlayer>().loopPointReached += CheckOver;
-    }
     void Update()
     {
-        if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey("enter") ||pressed)
-        {
-            animatorPlayButton.SetTrigger("Selected");
-            StartCoroutine("PlayPressed");
-        }
+        InputController();
+        SceneFade();
+    }
+    public void SceneFade()
+    {
         if (Fade.faded)
         {
             Fade.faded = false;
             SceneManager.LoadScene(SceneIndex);
         }
     }
+    public void InputController()
+    {
+        if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey("enter") || pressed)
+        {
+            animatorPlayButton.SetTrigger("Selected");
+            StartCoroutine("PlayPressed");
+        }
+    }
     public void Play()
     {
         pressed = true;
+    }
+    public void Credits()
+    {
+        StartCoroutine("CreditsPressed");
     }
     public void Exit()
     {
@@ -42,14 +47,15 @@ public class MainMenu : MonoBehaviour
         SceneIndex = SceneToTransition;
         animatorScene.SetTrigger("FadeOut");
     }
-    void CheckOver(VideoPlayer vp)
-    {
-        videoEnded = true;
-    }
     IEnumerator PlayPressed()
     {
-
         pressed = false;
+        yield return new WaitForSeconds(1);
+        FadeLevel(SceneManager.GetActiveScene().buildIndex + 2);
+        yield return null;
+    }
+    IEnumerator CreditsPressed()
+    {
         yield return new WaitForSeconds(1);
         FadeLevel(SceneManager.GetActiveScene().buildIndex + 1);
         yield return null;
